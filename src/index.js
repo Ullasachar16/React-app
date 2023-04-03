d4b4a1b2e74a28257ef480dde00b9389d62efeb7
Ullasachar16 <ullasachar16@gmail.com>
2023-04-03 11:11:26 +0530

    checking

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render("<h1>Hello World!!!</h1>",document.getElementById("root"));
 
